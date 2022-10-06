ea8cac14e0921ddeb009d35e8e3fd9a94f977092
MuliaSahpira <muliasahpira23@mhs.mdp.ac.id>
2022-10-06 17:02:21 +0700

    kuis_2125250077

---
+++ app/src/main/java/com/if3a/kuis_2125250077/MainActivity.java
@@ -0,0 +1,40 @@
+package com.if3a.kuis_2125250077;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogin, btnSignup, btnWww;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignup = findViewById(R.id.btn_signup);
+        btnWww = findViewById(R.id.btn_www);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWww.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("www.palembang.go.id"));
+                startActivity(intent);
+            }
+        });
+    }
+}
